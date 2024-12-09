f3d838f3693f2bcef5d03dfaace6130463fa8dc6
jamesjoy-2k24 <jamesjoy2k24@gmail.com>
2024-12-09 17:28:25 +0530

    modules 3 finished and excersice in for loop

---
+++ for_loop.php
@@ -0,0 +1,38 @@
+<?php
+// 01. Sum of First 10 Natural Numbers
+$total = 0;
+for ($i = 0; $i <= 10; $i++) {
+    $total = $total + $i;
+    }
+echo $total . "<br>";
+
+// 02. Printing Even Numbers Between 1 and 20
+for ($i = 0; $i < 20; $i++) {
+    if ($i % 2 === 0) {
+        echo $i . ",";
+        }
+    }
+
+// 03. Multiplication Table
+$input = 3;
+for ($i = 0; $i <= 12; $i++) {
+    $result = $i * $input;
+    echo "$i * $input = " . $result . "<br>";
+    }
+
+// 04. Factorial Calculation
+$total = 1;
+for ($i = 1; $i <= 5; $i++) {
+    $total = $total * $i;
+    }
+echo $total . "<br>";
+
+// 05. Array Element Sum
+$numArray = array('')
+
+// 06. Reverse Array
+// 07. Pattern Printing
+// 08. Prime Numbers
+// 09. FizzBuzz
+// 10. Nested Loops - Multiplication Table
+?>
